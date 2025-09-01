d66d0a6af552e3076d959b7f006d0ad4854bd239
Shadi Shadab <46791602+ShadiShadab@users.noreply.github.com>
2025-09-01 15:45:23 +0300

    Delete 2_data_preparation/ASOS_GraphReturns/check_price_column.py

--- 2_data_preparation/ASOS_GraphReturns/check_price_column.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Load the product nodes data
-product_nodes = pd.read_pickle('C:/Users/ADMIN/ET6-CDSP-group-17-repo/1_datasets/ASOS_GraphReturns/CSV Files/product_nodes_training.p')
-
-# Print the columns
-print(product_nodes.columns.tolist())
